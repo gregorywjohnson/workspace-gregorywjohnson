f902d2e20c5f4d28187063798c35cee774345326
Greg Johnson <gregory.w.johnson@gmail.com>
2017-04-26 14:47:08 +0000

    Made some changes to ChiliPeppr myWorkspace using Cloud9

--- workspace.js
+++ workspace.js
@@ -40,7 +40,7 @@ cpdefine("inline:com-chilipeppr-workspace-gregorywjohnson", ["chilipeppr_ready"]
          */
         id: "com-chilipeppr-workspace-gregorywjohnson", // Make the id the same as the cpdefine id
         name: "Workspace / gregorywjohnson", // The descriptive name of your widget.
-        desc: `A ChiliPeppr Workspace gregorywjohnson.`,
+        desc: `A ChiliPeppr workspace that maps 2D contours to 3D surfaces.`,
         url: "(auto fill by runme.js)", // The final URL of the working widget as a single HTML file with CSS and Javascript inlined. You can let runme.js auto fill this if you are using Cloud9.
         fiddleurl: "(auto fill by runme.js)", // The edit URL. This can be auto-filled by runme.js in Cloud9 if you'd like, or just define it on your own to help people know where they can edit/fork your widget
         githuburl: "(auto fill by runme.js)", // The backing github repo
